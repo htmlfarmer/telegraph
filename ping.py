4816da9dd06cf16a7313503dc6fbd3c6288be5aa
Asher Martin <ashermartin@gmail.com>
2015-08-28 00:16:51 -0400

    update timing info

--- ping.py
+++ ping.py
@@ -16,7 +16,7 @@ if(len(sys.argv) > 1):
 else:
     host= "boston.com" # should be located in Germany about 100 ms from USA
 
-
+"""
 # ping: to see the total time along the route
 pnum = 5 # number of pings to test
 pingtext = ""
@@ -51,9 +51,59 @@ route = {"ip" : host, # TODO ask Todd maybe has ideas?
         }
 
 print route # verify all the timings are correct for the trace
-
+"""
 # traceroute: to collect timing info along this route
 
+# function for calculating standard deviation
+def sdev(data, avg):
+    tdev = 0
+    for dev in data:
+        tdev += abs(dev-avg)
+    return tdev/len(data)
+
+# Todd: We would like to do something like this...
+trace = []
+def append_trace(ip, host, times):
+    minimum = min(times)
+    maximum = max(times)
+    average = sum(times)/len(times)
+    variance = sdev(times, average)
+
+    ping = {"ip" : ip,
+        "host" : host,
+        "times": times,
+        "min" : minimum,
+        "avg" : average,
+        "max" : maximum,
+        "sdev" : variance
+        #"address" : address,
+        #"lat" : lat,
+        #"lng" : lng
+        }
+    trace.append(ping)
+
+def parse_trace(line):
+    # get all the ip address for the route
+    matcher = re.compile("(?<=\()\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?=\) )")
+    ip = matcher.findall(line)
+
+    # get all the text domain names for the route
+    matcher = re.compile("(?<=  )[a-zA-Z0-9\-\.]+\.[a-zA-Z0-9]{1,5}(?= \()")
+    host = matcher.findall(line)
+
+    # get all the timing information for the route
+    matcher = re.compile("\d{1,5}\.\d{1,3}(?= ms)")
+    times = matcher.findall(line)
+
+    if len(ip) != 0 and len(host) != 0 and len(times) >= 1:
+        for i in range(len(times)):
+            times[i] = float(times[i]) # convert time to a number
+        print ip
+        print host
+        print times
+        append_trace(ip, host, times)
+
+# now run a trace route since all the functions are setup
 tnum = 5
 traceroute = subprocess.Popen(
     ["traceroute", "-q", str(tnum), host],
@@ -70,40 +120,9 @@ while True:
   # if we see 3 or more *'s in a row break
   if line != '' and (re.search('(\* ){3,}', line) is None):
     #the real code does filtering here
+    parse_trace(line)
     print line.rstrip() # remove return at end of line
   else:
     break
-
-# get all the ip address for the route
-matcher = re.compile("(?<=\()\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?=\) )")
-ips = matcher.findall(tracetext)
-print ips #print tracetext
-
-# get all the timing information for the route
-matcher = re.compile("\d{1,3}\.\d{1,3}(?= ms)")
-times = matcher.findall(tracetext)
-print times
-
-# get all the text domain names for the route
-matcher = re.compile("(?<=  )[a-zA-Z0-9\-\.]+\.[a-zA-Z0-9]{1,5}(?= \()")
-hosts = matcher.findall(tracetext)
-print hosts
-
-# Todd: We would like to do something like this...
-trace = []
-for index, ip in enumerate(ips):
-
-    ping = {"ip" : ip,
-        "host" : hosts[index]
-        #"min" : min,
-        #"avg" : avg,
-        #"max" : max,
-        #"dev" : dev,
-        #"address" : address,
-        #"lat" : lat,
-        #"lng" : lng
-        }
-
-    trace.append(ping)
 
 print trace
